4f39087646871d2f4f0802faeba176232762e759
Mark <kozlovmark@gmail.com>
2015-12-25 21:33:39 -0800

    Fixed block detection

--- src/main/java/com/mark/nbgui/EventHandlerCommon.java
+++ src/main/java/com/mark/nbgui/EventHandlerCommon.java
@@ -7,19 +7,22 @@ import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class EventHandlerCommon {
+	
+	
 	@SubscribeEvent
     public void openGUI(PlayerInteractEvent event) {
     	if (event.action == PlayerInteractEvent.Action.RIGHT_CLICK_BLOCK){
     		World world = event.world;
     		Block block = world.getBlockState(event.pos).getBlock();
-    		String blocktype = "+block";
+    		String blocktype = block.toString();
     		EntityPlayer player = event.entityPlayer;
-    		if (blocktype == "Block{minecraft:noteblock}") {
+    		if (blocktype.equals("Block{minecraft:noteblock}")) {
     			player.openGui(NBGUI.instance, GUI.GUI_ID, world, event.pos.getX(), event.pos.getY(), event.pos.getZ());
     		}
     		//DEBUG
-    		System.out.println("Position:" + event.pos);
-    		System.out.println("Block:" + block);
+    		System.out.println("POS: " +event.pos);
+    		System.out.println("BLOCK: " +block);
+    		System.out.print(blocktype);
     	}
     }
 }
