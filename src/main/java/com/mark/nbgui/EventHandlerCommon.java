aa337ba85755f740b1a6f3e3f56fd114a1e11cb0
Mark <kozlovmark@gmail.com>
2015-12-25 23:41:11 -0800

    Compare directly

--- src/main/java/com/mark/nbgui/EventHandlerCommon.java
+++ src/main/java/com/mark/nbgui/EventHandlerCommon.java
@@ -2,6 +2,7 @@ package com.mark.nbgui;
 
 import net.minecraft.block.Block;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
 import net.minecraft.world.World;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -14,15 +15,16 @@ public class EventHandlerCommon {
     	if (event.action == PlayerInteractEvent.Action.RIGHT_CLICK_BLOCK){
     		World world = event.world;
     		Block block = world.getBlockState(event.pos).getBlock();
-    		String blocktype = block.toString();
     		EntityPlayer player = event.entityPlayer;
-    		if (blocktype.equals("Block{minecraft:noteblock}")) {
+    		if (block == Blocks.noteblock) {
     			player.openGui(NBGUI.instance, GUI.GUI_ID, world, event.pos.getX(), event.pos.getY(), event.pos.getZ());
     		}
     		//DEBUG
     		System.out.println("POS: " +event.pos);
     		System.out.println("BLOCK: " +block);
-    		System.out.print(blocktype);
+    		
+    		//Note pitch = NoteBlockEvent.getNote();
+    		//String note = d;
     	}
     }
 }
